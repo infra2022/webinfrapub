c5276d1f39cab02aaffa1b9551597a2407ddd59d
Your Name <you@example.com>
2022-11-04 14:22:45 -0400

    add

--- webapp/modules/sec-groups/output.tf
+++ webapp/modules/sec-groups/output.tf
@@ -4,6 +4,3 @@ output "webserver_sec_grp_id" {
 output "load_balancer_sec_grp_id" {
   value = aws_security_group.loadbalancer-sg.id
 }
-output "jenkins_sec_grp_id" {
-  value = aws_security_group.jenkins-sg.id
-}
